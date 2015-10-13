295a2b7a1d863778df5551a5df94b175b8e64bc6
Alumno <Alumno@10.207.98.142>
2015-10-13 21:34:22 +0200

    Coche Test

---
+++ pr02-fuentes/src/ud/prog3/pr02/JUnitCoche.java
@@ -0,0 +1,36 @@
+package ud.prog3.pr02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitCoche {
+
+	Coche coche = new Coche();
+	
+	@Test
+	public void testFuerzaAceleracionAtras() {
+		double[] tablaVel = { -500, -425, -300, -250, -200, -100, 0, 125, 250, 500, 1100 };
+		double[] tablaFuerza = { 0, 0.5, 1, 1, 1, 0.65, 0.3, 0.575, 0.85, 0.85, 0.85 };
+		for (int i=0;i<tablaVel.length;i++) {
+			coche.setVelocidad( tablaVel[i] );
+			assertEquals( "Velocidad " + tablaVel[i], tablaFuerza[i]*coche.FuerzaAtras, coche.FuerzaAceleraionAtras(), 0.0000001 );
+		}
+	}
+	
+	@Test
+	public void testFuerzaAceleracionAdelante() {
+		double[] tablaVel = { -500, -425, -300, -250, -200, -100, 0, 125, 250, 500, 1100 };
+		double[] tablaFuerza = { 0, 0.5, 1, 1, 1, 0.65, 0.3, 0.575, 0.85, 0.85, 0.85 };
+		for (int i=0;i<tablaVel.length;i++) {
+			coche.setVelocidad( tablaVel[i] );
+			assertEquals( "Velocidad " + tablaVel[i], tablaFuerza[i]*coche.FuerzaAdelante, coche.fuerzaAceleracionAdelante(), 0.0000001 );
+		}
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
